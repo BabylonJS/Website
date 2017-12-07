784d3510231562ebe4dc9e78e89ef557546559f4
David Catuhe <david.catuhe@live.fr>
2017-12-07 10:55:21 -0800

    Removed theCar binary mode

--- Demos/TheCar/demo.js
+++ Demos/TheCar/demo.js
@@ -1,10 +1,10 @@
 ﻿var demo = {
     scene: "TheCar",
     incremental: false,
-    binary: true,
+    binary: false,
     doNotUseCDN: false,
     collisions: true,
-    offline: false,
+    offline: true,
     onload: function () {
         scene.getMeshByName("C-Max_Pneu_arrière_gauche").material.bumpTexture = null;
         scene.getMeshByID("b73467cc-d1b0-4b8b-a767-12a95e0e28cf").alphaIndex = 0;
