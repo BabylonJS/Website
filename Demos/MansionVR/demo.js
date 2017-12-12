5ab140fc1d072b77c43aaba392461789beb53b29
QuentinRillet <quentin.rillet@gmail.com>
2017-12-12 16:55:33 +0100

    VR on HillValley and Mansion, reorganize folder

---
+++ Demos/MansionVR/demo.js
@@ -0,0 +1,12 @@
+﻿var demo = {
+    scene: "Mansion",
+    incremental: false,
+    binary: false,
+    doNotUseCDN: true,
+    collisions: true,
+    offline: false,
+    onload: function () {
+        var VRHelper = scene.createDefaultVRExperience();
+        VRHelper.enableTeleportation({ floorMeshName: "Allée" });
+    }
+};
