5a1b9029ccf561fd179440b59077a6a9b9889beb
David Catuhe <david.catuhe@live.fr>
2017-12-12 19:44:42 -0800

    fix race condition

--- Demos/FlightHelmet/index.js
+++ Demos/FlightHelmet/index.js
@@ -18,6 +18,26 @@
 		scene.activeCamera.alpha = 2.5;
 		scene.activeCamera.beta = 1.5;
 		scene.activeCamera.useAutoRotationBehavior = true;
+
+		var light = new BABYLON.DirectionalLight("light1", new BABYLON.Vector3(2, -3, -1), scene);
+		light.position = new BABYLON.Vector3(-20, 20, 6);
+		var generator = new BABYLON.ShadowGenerator(512, light);
+		generator.useBlurExponentialShadowMap = true;
+		generator.blurKernel = 32;
+		generator.getShadowMap().refreshRate = BABYLON.RenderTargetTexture.REFRESHRATE_RENDER_ONCE;
+
+		for (var i = 0; i < scene.meshes.length; i++) {
+			generator.addShadowCaster(scene.meshes[i]);
+		}
+
+		var helper = scene.createDefaultEnvironment({
+			skyboxSize: 1500,
+			groundShadowLevel: 0.6,
+		});
+
+		helper.setMainColor(new BABYLON.Color3(.42, .41, .33));
+
+		scene.meshes[0].position.y -= 0.5;		
 	});
 
 	return scene;
