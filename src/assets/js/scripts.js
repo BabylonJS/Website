e49c4b8106a2f41079d25079a9bd4cf266dcfeb5
Ryan Tremblay <ryan.tremblay@microsoft.com>
2025-03-06 16:45:36 -0800

    Add link to viewer configurator
    Update viewer version

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -271,7 +271,7 @@ window.onload = function () {
 <div class="img-holder">
  <babylon-viewer
             source="${manifest.baseUrl}/${demo.filename}"
-            environment="https://cdn.jsdelivr.net/npm/@babylonjs/viewer@preview/assets/photoStudio.env"
+            environment="https://cdn.jsdelivr.net/npm/@babylonjs/viewer/assets/photoStudio.env"
           >
           </babylon-viewer>
         </div>
