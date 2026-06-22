ee2d4f62b3ce0c89ff3f6b93c49aef3b656f1359
Raanan Weber <raananw+github@gmail.com>
2026-06-22 15:03:21 +0200

    fix(pure-demos): register Ray and rawTexture for Babylon 9.13 bump
    Lens needs RegisterRay() (LensFlareSystem) and PPRef needs
    RegisterEnginesExtensionsEngineRawTexture() (RefractionPostProcess);
    RegisterStandardEngineExtensions no longer covers rawTexture.

--- src/pureCompiledDemos/PPRef/main.ts
+++ src/pureCompiledDemos/PPRef/main.ts
@@ -1,7 +1,8 @@
-import { RegisterStandardEngineExtensions } from "@babylonjs/core/pure";
+import { RegisterEnginesExtensionsEngineRawTexture, RegisterStandardEngineExtensions } from "@babylonjs/core/pure";
 import { runDemo } from "../shared/demoRunner";
 import { createPostProcessRefractionScene } from "./scene";
 
 RegisterStandardEngineExtensions();
+RegisterEnginesExtensionsEngineRawTexture();
 
 runDemo({ createScene: createPostProcessRefractionScene });
